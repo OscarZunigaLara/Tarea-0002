d2dc655e0a57b26fa2fe18903e3c62ee14480a11
Tandoori7 <oscaritohalo@hotmail.com>
2017-08-21 11:45:25 -0500

    Aquí esta el codigo.

--- cordenadas.py
+++ cordenadas.py
@@ -12,6 +12,6 @@ yval = int(input("¿Cuál es el valor de Y?"))
 r = (xval**2 + yval**2)**0.5
 angulo =(math.atan2(yval, xval)) * 180 /math.pi
 
-    print("El valor de R es: ", r)
-    print("El angulo es: ", angulo)
+print("El valor de R es: ", r)
+print("El angulo es: ", angulo)
 
